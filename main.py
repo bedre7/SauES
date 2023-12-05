51ed8e84407c58be462711685f5bb332a5657f4b
Bedru Umer <bedru2019@gmail.com>
2023-12-05 21:34:11 +0300

    improve user interface on console

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from SauES import *
 from time import time
 
 if __name__ == '__main__':
+    print("Enter key: ", end="")
     key = input()
-    operation, text = input().split()
+    print("Enter operation (Encrypt/Decrypt): ", end="")
+    operation = input()
+    print("Enter text: ", end="")
+    text = input()
 
     sauES = SauES(key)
-    if operation == 'Encrypt':
+    if operation.lower() == 'encrypt':
         # record the time before encryption
         start_time = time()
-        print(sauES.encrypt(text))
+        print("Encrypted text: ", sauES.encrypt(text))
         # record the time after encryption
         end_time = time()
         # print the time taken to encrypt in milliseconds
         print(f'Encoded in {round((end_time - start_time) * 1000, 2)}ms') 
-    elif operation == 'Decrypt':
+    elif operation.lower() == 'decrypt':
         start_time = time()
-        print(sauES.decrypt(text))
+        print("Decrypted text: ", sauES.decrypt(text))
         end_time = time()
         print(f'Decoded in {round((end_time - start_time) * 1000, 2)}ms')
